acda4705aa68253cbdc5dc0454f98191ef0713e0
kulonik <annakhmv@gmail.com>
2024-06-14 21:47:15 +0500

    Fixed bug with displaying score in html file

--- index.js
+++ index.js
@@ -2,67 +2,87 @@ const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
 const resultDisplay = document.getElementById('result');
+const humanScoreDisplay = document.getElementById('player-score');
+const computerScoreDisplay = document.getElementById('computer-score');
+const looser = "You lose!";
+const winner = "You win!";
+
+let computerScore = 0;
+let humanScore = 0;
+let humanChoice;
 
 rockButton.addEventListener('click', () => playRound('rock'));
 paperButton.addEventListener('click', () => playRound('paper'));
 scissorsButton.addEventListener('click', () => playRound('scissors'));
 
-const looser = "You lose!";
-const winner = "You win!";
 
 function playRound(humanChoice) {
 const choiceArr = ['rock', 'paper', 'scissors'];
-let computerChoice = choiceArr[Math.floor(Math.random() * 3)];   
-    if(humanChoice === choiceArr[0]) {
-        if(computerChoice === choiceArr[1]) {
+let computerChoice = choiceArr[Math.floor(Math.random() * 3)];  
+
+    if(humanChoice === 'rock') {
+        if(computerChoice === 'paper') {
             computerScore++;
-            return resultDisplay.textContent = `${looser} Paper beats Rock!`;
-        } else if (computerChoice === choiceArr[2]) {
+            resultDisplay.textContent = `${looser} Paper beats Rock!`;
+            humanScoreDisplay.textContent = `Player Score: ${humanScore}`;
+            computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+                } else if (computerChoice === choiceArr[2]) {
             humanScore++;
-            return resultDisplay.textContent = `${winner} Rock beats Scissors!`;
-        } else {
+             resultDisplay.textContent = `${winner} Rock beats Scissors!`;
+             humanScoreDisplay.textContent = `Player Score: ${humanScore}`;
+             computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+                 } else {
             return resultDisplay.textContent = "It\'s a draw!";
         }
     } else if (humanChoice === choiceArr[1]) {
         if (computerChoice === choiceArr[0]) {
             humanScore++;
-            return resultDisplay.textContent = `${winner} Paper beats Rock!`;
+             resultDisplay.textContent = `${winner} Paper beats Rock!`;
+             humanScoreDisplay.textContent = `Player Score: ${humanScore}`;
+             computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+         
         } else if (computerChoice === choiceArr[2]) {
             computerScore++;
-            return resultDisplay.textContent = `${looser} Scissors beat Paper!`;
-        } else {
-            return resultDisplay.textContent = "It\'s a draw!";
-        }
+             resultDisplay.textContent = `${looser} Scissors beat Paper!`;
+             humanScoreDisplay.textContent = `Player Score: ${humanScore}`;
+             computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+                 } else {
+             resultDisplay.textContent = "It\'s a draw!";
+             humanScoreDisplay.textContent = `Player Score: ${humanScore}`;
+             computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+                 }
     } else if (humanChoice === choiceArr[2]) {
         if (computerChoice === choiceArr[0]) {
             computerScore++;
-            return resultDisplay.textContent = `${looser} Rock beats Scissors!`;
-        } else if (computerChoice === choiceArr[1]) {
+             resultDisplay.textContent = `${looser} Rock beats Scissors!`;
+             humanScoreDisplay.textContent = `Player Score: ${humanScore}`;
+             computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+                 } else if (computerChoice === choiceArr[1]) {
             humanScore++;
-            return resultDisplay.textContent = `${winner} Scissors beat Paper!`;
-        } else {
-            return resultDisplay.textContent = "It\'s a draw!";
-        }
+             resultDisplay.textContent = `${winner} Scissors beat Paper!`;
+             humanScoreDisplay.textContent = `Player Score: ${humanScore}`;
+             computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+                 } else {
+             resultDisplay.textContent = "It\'s a draw!";
+             humanScoreDisplay.textContent = `Player Score: ${humanScore}`;
+             computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+                 }
     }
-
+    console.log(`Player Score: ${humanScore}`);
+    console.log(`Computer Score: ${computerScore}`);
     humanScoreDisplay.textContent = `Player Score: ${humanScore}`;
     computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
 };
-
-    let computerScore = 0;
-    let humanScore = 0;
-    const humanScoreDisplay = document.getElementById('player-score');
-    const computerScoreDisplay = document.getElementById('computer-score');
+console.log(playRound(humanChoice));
+/*
 
 function playGame() {
-
     for(let i = 0; i < 5; i++) {
        // const humanSelection = getHumanChoice();
-        const computerSelection = computerChoice();
+        const computerSelection = computerChoice;
         // playRound(humanSelection, computerSelection);
         playRound(computerSelection);
     }
-
 
     if(computerScore < humanScore) {
         return "You are a winner!";
@@ -72,5 +92,4 @@ function playGame() {
         return "You are tie!";
     }
 };
-
-console.log(playGame());
+*/
